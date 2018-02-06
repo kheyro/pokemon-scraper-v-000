9032e568d76716d1e4a3dad225a7ed490d2c6996
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 12:31:42 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+
+  def initialize(name, type, db)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save
+    ins = @db.prepare("INSERT INTO pokemon (name, type) VALUES (?, ?)")
+  end
+
+  def self.find
+  end
 end
